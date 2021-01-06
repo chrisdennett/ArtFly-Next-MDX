8ac452c4e4ee4c3a819887d95783ca47a3f21459
chrisdennett <chrisdennett@gmail.com>
2021-01-06 14:25:34 +0000

    Added cloudinary for images

--- components/Img.js
+++ components/Img.js
@@ -1,10 +1,39 @@
 import React from "react";
 import styles from "./img.module.css";
+import { Image, Placeholder } from "cloudinary-react";
 
 const Img = ({ src, w, h, alt }) => {
   return (
     <>
-      <img src={src} width={w} height={h} alt={alt} className={styles.img} />
+      <Image
+        alt={alt}
+        className={styles.img}
+        cloudName="artfly"
+        publicId={`artfly-projects/${src}`}
+        loading="lazy"
+        dpr="auto"
+        responsive
+        width="auto"
+        crop="scale"
+        responsiveUseBreakpoints="true"
+      >
+        <Placeholder type="pixelate" />
+      </Image>
+
+      {/* <img
+        src={`https://res.cloudinary.com/artfly/image/upload/c_thumb,w_200,g_face/v1609227691/artfly-projects/${src}`}
+        width={200}
+        height={h}
+        alt={alt}
+        className={styles.img}
+      />
+      <img
+        src={`https://res.cloudinary.com/artfly/image/upload/v1609227691/artfly-projects/${src}`}
+        width={w}
+        height={h}
+        alt={alt}
+        className={styles.img}
+      /> */}
     </>
   );
 };
